2476dcaa845ee75cea8b1619d7e6be6066028e3f
Tim Douglas <neurotech@gmail.com>
2019-06-07 18:26:53 +1000

    Added loading/success/fail visualisations to RightGIF command
    Resolves #25

--- src/components/RightGIF.js
+++ src/components/RightGIF.js
@@ -1,4 +1,20 @@
 module.exports = function rightGIFComponent(fastn, app) {
+  var loadingText = fastn(
+    "div",
+    { hidden: fastn.binding("componentData.url", url => url), class: "rightgif-loading-text" },
+    fastn.binding("componentData.status", status => {
+      if (!status) {
+        return "Enter a search term to find the right GIF.";
+      }
+      if (status === "loading") {
+        return "Getting the right GIF...";
+      }
+      if (status === "error") {
+        return "Encountered an error trying to find your GIF.";
+      }
+    })
+  );
+
   var imageElement = fastn("img", {
     hidden: fastn.binding("componentData.url", url => !url),
     class: fastn.binding("componentData.url", url => url && "fadein"),
@@ -10,6 +26,7 @@ module.exports = function rightGIFComponent(fastn, app) {
     {
       class: fastn.binding("componentData.status", status => ["rightgif-image", status])
     },
+    loadingText,
     imageElement
   ).attach(app.state);
 
